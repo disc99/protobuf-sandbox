7f4e06239844bef8cb1a4ae6313485818cbfe7be
disc99 <disc99.g@gmail.com>
2020-01-04 13:06:48 +0900

    Add protobuf json codec

--- spring-grpc-client-on-webflux/src/main/java/io/disc99/grpc/spring/client/SpringGRpcClientOnWebFluxApplication.java
+++ spring-grpc-client-on-webflux/src/main/java/io/disc99/grpc/spring/client/SpringGRpcClientOnWebFluxApplication.java
@@ -1,6 +1,7 @@
 package io.disc99.grpc.spring.client;
 
 import com.google.protobuf.InvalidProtocolBufferException;
+import com.google.protobuf.Message;
 import com.google.protobuf.util.JsonFormat;
 import io.disc99.echo.v1.Echo;
 import io.disc99.echo.v1.EchoServiceGrpc;
@@ -12,10 +13,17 @@ import org.reactivestreams.Publisher;
 import org.reactivestreams.Subscriber;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.codec.CodecCustomizer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.ResolvableType;
+import org.springframework.core.codec.DecodingException;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.core.io.buffer.DataBufferFactory;
+import org.springframework.core.io.buffer.DataBufferUtils;
 import org.springframework.http.MediaType;
+import org.springframework.http.codec.CodecConfigurer;
 import org.springframework.http.codec.protobuf.ProtobufDecoder;
-import org.springframework.stereotype.Component;
+import org.springframework.http.codec.protobuf.ProtobufEncoder;
 import org.springframework.util.MimeType;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
@@ -26,9 +34,14 @@ import reactor.core.publisher.BaseSubscriber;
 import reactor.core.publisher.Mono;
 
 import javax.annotation.Nonnull;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Method;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 
 @SpringBootApplication
@@ -44,16 +57,72 @@ public class SpringGRpcClientOnWebFluxApplication {
         return RouterFunctions.route()
                 .POST("/echo_with_text", echoHandler::getEcho)
                 .POST("/echo_with_json", echoHandler::geEcho2)
+                .POST("/echo_with_protobuf_json_codec", echoHandler::geEcho3)
                 .build();
     }
 
-    @Component
+    @Bean
+    CodecCustomizer protobufJsonCodec() {
+        return configurer -> {
+            CodecConfigurer.CustomCodecs customCodecs = configurer.customCodecs();
+            customCodecs.decoder(new ProtobufJsonDecoder());
+            customCodecs.encoder(new ProtobufJsonEncoder());
+        };
+    }
+
     static class ProtobufJsonDecoder extends ProtobufDecoder {
-        static final List<MimeType> MIME_TYPES = Collections.singletonList(new MimeType("application", "json"));
+        private static final List<MimeType> MIME_TYPES = Collections.singletonList(new MimeType("application", "json"));
 
         @Override
-        protected List<MimeType> getMimeTypes() {
-            return MIME_TYPES;
+        public boolean canDecode(ResolvableType elementType, MimeType mimeType) {
+            return Message.class.isAssignableFrom(elementType.toClass()) && MIME_TYPES.contains(mimeType);
+        }
+
+        @Override
+        public Message decode(DataBuffer dataBuffer, ResolvableType targetType, MimeType mimeType, Map<String, Object> hints) throws DecodingException {
+            try {
+                Message.Builder builder = getMessageBuilder(targetType.toClass());
+                JsonFormat.parser().merge(new InputStreamReader(dataBuffer.asInputStream()), builder);
+                return builder.build();
+            } catch (IOException ex) {
+                throw new DecodingException("I/O error while parsing input stream", ex);
+            } catch (Exception ex) {
+                throw new DecodingException("Could not read Protobuf message: " + ex.getMessage(), ex);
+            } finally {
+                DataBufferUtils.release(dataBuffer);
+            }
+        }
+
+        private static Message.Builder getMessageBuilder(Class<?> clazz) throws Exception {
+            Method method = clazz.getMethod("newBuilder");
+            return (Message.Builder) method.invoke(clazz);
+        }
+    }
+
+    static class ProtobufJsonEncoder extends ProtobufEncoder {
+        private static final List<MimeType> MIME_TYPES = Collections.singletonList(new MimeType("application", "json"));
+
+        @Override
+        public boolean canEncode(ResolvableType elementType, MimeType mimeType) {
+            return Message.class.isAssignableFrom(elementType.toClass()) && MIME_TYPES.contains(mimeType);
+        }
+
+        @Override
+        public DataBuffer encodeValue(Message message, DataBufferFactory bufferFactory, ResolvableType valueType, MimeType mimeType, Map<String, Object> hints) {
+            DataBuffer buffer = bufferFactory.allocateBuffer();
+            boolean release = true;
+            try {
+                String json = JsonFormat.printer().print(message);
+                buffer.write(json, UTF_8);
+                release = false;
+                return buffer;
+            } catch (IOException ex) {
+                throw new IllegalStateException("Unexpected I/O error while writing to data buffer", ex);
+            } finally {
+                if (release) {
+                    DataBufferUtils.release(buffer);
+                }
+            }
         }
     }
 
@@ -112,6 +181,37 @@ public class SpringGRpcClientOnWebFluxApplication {
                             return Mono.error(e);
                         }
                     });
+        }
+
+        // gRPC request calling and response json mapping
+        @Nonnull
+        public Mono<ServerResponse> geEcho3(ServerRequest serverRequest) {
+            return Mono.just(Echo.newBuilder())
+
+                    // Parse Request
+                    .flatMap(builder -> serverRequest.bodyToMono(Echo.class).map(Echo::toBuilder))
+                    .map(builder -> {
+                        boolean hasPathVariable = false; // TODO
+                        boolean hasParameter = false; // TODO
+
+                        if (hasPathVariable) {
+
+                        }
+
+                        if (hasParameter) {
+
+
+                        }
+
+                        Echo message = builder.build();
+                        return message;
+                    })
+
+                    // Call gRPC
+                    // TODO
+
+                    // Create Response
+                    .flatMap(res -> ok().contentType(MediaType.APPLICATION_JSON).bodyValue(res));
         }
     }
 
--- spring-grpc-client-on-webflux/src/test/java/io/disc99/grpc/spring/client/SpringGRpcClientOnWebFluxApplicationTest.java
+++ spring-grpc-client-on-webflux/src/test/java/io/disc99/grpc/spring/client/SpringGRpcClientOnWebFluxApplicationTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -26,20 +27,31 @@ class SpringGRpcClientOnWebFluxApplicationTest {
     @Test
     public void testEchoWithText() throws Exception {
         this.webTestClient.post()
-                .uri("/echo_with_text") //
+                .uri("/echo_with_text")
                 .bodyValue("message\": \"Hello\"")
-                .exchange() //
-                .expectStatus().isOk() //
+                .exchange()
+                .expectStatus().isOk()
                 .expectBody(String.class).isEqualTo("message: \"Hello!\"\n");
     }
 
     @Test
     public void testEchoWithJson() throws Exception {
         this.webTestClient.post()
-                .uri("/echo_with_json") //
+                .uri("/echo_with_json")
                 .bodyValue("{\"message\":\"Hello\"}")
-                .exchange() //
-                .expectStatus().isOk() //
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(String.class).isEqualTo("{\n  \"message\": \"Hello\"\n}");
+    }
+
+    @Test
+    public void testEchoWithProtobufJsonCodec() throws Exception {
+        this.webTestClient.post()
+                .uri("/echo_with_protobuf_json_codec")
+                .contentType(new MediaType("application", "json"))
+                .bodyValue("{\"message\":\"Hello\"}")
+                .exchange()
+                .expectStatus().isOk()
                 .expectBody(String.class).isEqualTo("{\n  \"message\": \"Hello\"\n}");
     }
 }
